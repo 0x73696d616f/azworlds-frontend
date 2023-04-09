cf8cb2dfc95ebacf1d9bb0d7ef2e02445963f561
0x73696d616f <116109421+0x73696d616f@users.noreply.github.com>
2023-04-09 03:43:57 +0100

    fix

--- component/charCardChain.js
+++ component/charCardChain.js
@@ -6,7 +6,7 @@ import abi from "../contracts/CharacterSale.json";
 
 const CharCardChain = (data) => {
   const [selectedGold, setSelectedGold] = useState(0);
-  
+
   const sendTo = async (index) => {
     data.setIsLoading(true);
     try {
@@ -22,8 +22,8 @@ const CharCardChain = (data) => {
       let chainId;
       if (index === "0") chainId = 10161 // Sepolia
       else if (index === "1") chainId = 10109  // Mumbai
-      else throw("Invalid index");
-      const tx = await characterSale.sendFrom(signerAddress, chainId, signerAddress, data.character.charId);
+      else throw ("Invalid index");
+      const tx = await characterSale.sendFrom(signerAddress, chainId, signerAddress, data.character.charId, ethers.utils.solidityPack(['uint16', 'uint256'],[1, 200000]), { value: ethers.utils.parseEther("0.3") });
       await tx.wait();
     }
     catch (err) {
@@ -45,50 +45,50 @@ const CharCardChain = (data) => {
       const characterSale = new ethers.Contract(characterSaleAddress, abi, signer);
       const tx = await characterSale.dropGold(data.character.charId, selectedGold);
       await tx.wait();
-    } catch(err) {
+    } catch (err) {
       console.log(err);
     }
     data.setIsLoading(false);
   }
 
   return (
-          <Card>
-            <Card.Image
-              src={data.character.img}
-              objectFit="cover"
-              alt="Card image background"
-            />
-            <Card.Body>
-              <Text size="small">Char ID: {data.character.charId}</Text>
-              <Text size="small">Level: {data.character.level}</Text>
-              <Text size="small">Power: {data.character.power}</Text>
-              <Text size="small">Chain: {data.character.currentChain}</Text>
-              <Popover>
-                <Popover.Trigger>
-                  <Button auto color="warning">Details</Button>
-                </Popover.Trigger>
-                <Popover.Content>
-                  <Text size="small">Buy Price: {data.character.buyPrice}</Text>
-                  <Text size="small">Equipped Items: {data.character.equippedItems.toString()}</Text>
-                  <div style={{display:"flex", flexDirection: "row", justifyContent: "center", alignItems: "center"}}>
-                    <Text size="small">Equipped Gold: {data.character.equippedGold}</Text> 
-                    <Button light auto color="warning" onClick={unequipGold} size="xs">Unequip Gold</Button>
-                    <Input auto type="number" onChange={(e) => setSelectedGold(e.target.value)} placeholder="Amount" size="xs"></Input>
-                  </div>
-                  <Text size="small">Owner: {data.character.owner}</Text>
-                  <Link color="warning" size="small" href={data.character.url}>Metadata</Link>
-                </Popover.Content>
-              </Popover>
-              <Spacer y={0.5}></Spacer>
-              < Dropdown>
-                <Dropdown.Button auto color="warning">Go to Chain</Dropdown.Button>
-                <Dropdown.Menu color="warning" aria-label="Static Actions" auto onAction={sendTo}>
-                  {data.character.currentChain !== "Sepolia" && <Dropdown.Item key={0}>Sepolia</Dropdown.Item>}
-                  {data.character.currentChain !== "Mumbai" && <Dropdown.Item key={1}>Mumbai</Dropdown.Item>}
-                </Dropdown.Menu>
-              </Dropdown>
-            </Card.Body>
-          </Card>
+    <Card>
+      <Card.Image
+        src={data.character.img}
+        objectFit="cover"
+        alt="Card image background"
+      />
+      <Card.Body>
+        <Text size="small">Char ID: {data.character.charId}</Text>
+        <Text size="small">Level: {data.character.level}</Text>
+        <Text size="small">Power: {data.character.power}</Text>
+        <Text size="small">Chain: {data.character.currentChain}</Text>
+        <Popover>
+          <Popover.Trigger>
+            <Button auto color="warning">Details</Button>
+          </Popover.Trigger>
+          <Popover.Content>
+            <Text size="small">Buy Price: {data.character.buyPrice}</Text>
+            <Text size="small">Equipped Items: {data.character.equippedItems.toString()}</Text>
+            <div style={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
+              <Text size="small">Equipped Gold: {data.character.equippedGold}</Text>
+              <Button light auto color="warning" onClick={unequipGold} size="xs">Unequip Gold</Button>
+              <Input auto type="number" onChange={(e) => setSelectedGold(e.target.value)} placeholder="Amount" size="xs"></Input>
+            </div>
+            <Text size="small">Owner: {data.character.owner}</Text>
+            <Link color="warning" size="small" href={data.character.url}>Metadata</Link>
+          </Popover.Content>
+        </Popover>
+        <Spacer y={0.5}></Spacer>
+        < Dropdown>
+          <Dropdown.Button auto color="warning">Go to Chain</Dropdown.Button>
+          <Dropdown.Menu color="warning" aria-label="Static Actions" auto onAction={sendTo}>
+            {data.character.currentChain !== "Sepolia" && <Dropdown.Item key={0}>Sepolia</Dropdown.Item>}
+            {data.character.currentChain !== "Mumbai" && <Dropdown.Item key={1}>Mumbai</Dropdown.Item>}
+          </Dropdown.Menu>
+        </Dropdown>
+      </Card.Body>
+    </Card>
   )
 }
 export default CharCardChain;
